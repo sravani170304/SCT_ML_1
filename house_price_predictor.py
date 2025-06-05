46a63f05aa4d20756b9bf3ee19bebbc8b5be9891
sravani170304 <148191959+sravani170304@users.noreply.github.com>
2025-06-05 19:56:51 +0530

    Update house_price_predictor.py

--- house_price_predictor.py
+++ house_price_predictor.py
@@ -1,1 +1,11 @@
+This is the core machine learning script that:
 
+Loads the dataset
+
+Uses LinearRegression from scikit-learn
+
+Trains the model using area, bedrooms, and bathrooms
+
+Evaluates model performance (MSE, R² score)
+
+Visualizes the actual vs predicted results
